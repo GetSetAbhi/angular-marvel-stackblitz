7c28175819933ffd93a0d5e614d5d2c67c04def9
GetSetAbhi <araina97@gmail.com>
2022-09-24 20:31:54 +0530

    1 file modified

--- src/app/services/marvel.service.ts
+++ src/app/services/marvel.service.ts
@@ -6,14 +6,12 @@ import { CharacterDataWrapper } from '../model/marvel-model.dto';
   providedIn: 'root',
 })
 export class MarvelService {
-  publicKey = '6493ff23f4f9b467be44a6e02463fbdd';
-
-  privateKey = '3ff767aded2356837a8e64f5287084357fc25212';
+  rootUrl = 'https://gateway.marvel.com:443/v1/public';
 
   constructor(private http: HttpClient) {}
 
   public getAllCharacters() {
-    let url = 'https://gateway.marvel.com:443/v1/public/characters';
+    let url = this.rootUrl + '/characters';
     return this.http.get<CharacterDataWrapper>(url);
   }
 }
